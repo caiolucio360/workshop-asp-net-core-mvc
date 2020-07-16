b397a83ca52558ae87a5acd001560071df8272aa
Caio Lucio <caioluci1@gmail.com>
2020-07-16 19:05:04 -0300

    Implementing simple search

---
+++ SalesWebMvc/Migrations/20200716212821_UpdateColumnStatus.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class UpdateColumnStatus : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MStatus",
+                table: "SalesRecord");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "SalesRecord");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Seller",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AddColumn<int>(
+                name: "MStatus",
+                table: "SalesRecord",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
